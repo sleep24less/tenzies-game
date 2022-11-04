9f73424128c6afc9ce03e31c26d953625e7d6545
sleepless <arnas.manucan@gmail.com>
2022-11-04 15:55:18 +0200

    Add and style dice comp, add tab icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import { useState } from 'react'
+import Die from './Die'
 
 function App() {
   return (
     <main className='main_box'>
-
+      <div className='dice_container'>
+        <Die value={1} />
+        <Die value={2} />
+        <Die value={3} />
+        <Die value={4} />
+        <Die value={5} />
+        <Die value={6} />
+        <Die value={7} />
+        <Die value={8} />
+        <Die value={9} />
+        <Die value={1} />
+      </div>
     </main>
   )
 }
---
+++ src/Die.jsx
@@ -0,0 +1,7 @@
+export default function Die(props) {
+    return (
+        <div className="die noselect">
+            {props.value}
+        </div>
+    )
+}
