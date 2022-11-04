4e35b0d3188d257f2c8f71a9269e3675aa8e5b56
sleepless <arnas.manucan@gmail.com>
2022-11-04 16:39:04 +0200

    Make dice values change at random with refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
-import { useState } from 'react'
+import React from 'react'
 import Die from './Die'
 
 function App() {
 
+  const [dice, setDice] = React.useState(allNewDice())
+
   function allNewDice() {
     const newDice = [];
-
     for (let i = 0; i < 10; i++) {
       newDice.push(Math.ceil(Math.random() * 6))
     }
     return newDice
   }
 
+  const diceElements = dice.map(die => <Die value={die} />)
+
   return (
     <main className='main_box'>
       <div className='dice_container'>
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
-        <Die value={5} />
-        <Die value={6} />
-        <Die value={7} />
-        <Die value={8} />
-        <Die value={9} />
-        <Die value={1} />
+        {diceElements}
       </div>
     </main>
   )
