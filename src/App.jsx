94e1ef56b35a4b0047c3ea6fa041407cef97e087
sleepless <arnas.manucan@gmail.com>
2022-11-04 16:21:23 +0200

    Add function to generate random dice values

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,16 @@ import { useState } from 'react'
 import Die from './Die'
 
 function App() {
+
+  function allNewDice() {
+    const newDice = [];
+
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6))
+    }
+    return newDice
+  }
+
   return (
     <main className='main_box'>
       <div className='dice_container'>
