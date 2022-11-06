5ba16d1f4f967f4ceb303d21e9c77ca5d21b8a97
sleepless <arnas.manucan@gmail.com>
2022-11-06 17:00:16 +0200

    Add functionality to hold dice

--- src/App.jsx
+++ src/App.jsx
@@ -6,37 +6,37 @@ function App() {
 
   const [dice, setDice] = React.useState(allNewDice())
 
+  const diceElements = dice.map(die => <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} />)
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid()
+    }
+  }
+
   function allNewDice() {
     const newDice = []
     for (let i = 0; i < 10; i++) {
-      newDice.push({
-        value: Math.ceil(Math.random() * 6),
-        isHeld: false,
-        id: nanoid()
-      })
+      newDice.push(generateNewDie())
     }
     return newDice
   }
 
   function rollDice() {
-    setDice(allNewDice())
+    setDice(oldDice => {
+      return oldDice.map(die => {
+        return die.isHeld === true ? die : generateNewDie()
+      })
+    })
   }
 
   function holdDice(id) {
     setDice(oldDice => oldDice.map(die => {
-      if (die.id === id) {
-        return {
-          ...die,
-          isHeld: !die.isHeld
-        }
-      }
-      else {
-        return die
-      }
+      return die.id === id ? { ...die, isHeld: !die.isHeld } : die
     }))
   }
-
-  const diceElements = dice.map(die => <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} />)
 
   return (
     <main className='main_box'>
