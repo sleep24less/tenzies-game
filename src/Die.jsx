97aacc94235f3baa24f89dd4171523b77ea1c63b
sleepless <arnas.manucan@gmail.com>
2022-11-07 15:24:00 +0200

    Minor fix

--- src/Die.jsx
+++ src/Die.jsx
@@ -5,7 +5,7 @@ export default function Die(props) {
 
     return (
         <div className="die noselect" style={style} onClick={props.holdDice}>
-            <img src={`../public/${props.value}.png`} alt={props.value} className='dice_img' />
+            <img src={`./public/${props.value}.png`} alt={props.value} className='dice_img' />
         </div>
     )
 }
